1382777d69835179c2080987af08e7ba22e88ae2
João Victor Souza Guedes <vcitorsguedes@hotmail.com>
2025-04-24 21:15:27 -0300

    questao02: adding button click logic

--- Questao02/app/src/main/java/com/joaovictorguedes/questao02/MainActivity.java
+++ Questao02/app/src/main/java/com/joaovictorguedes/questao02/MainActivity.java
@@ -1,6 +1,11 @@
 package com.joaovictorguedes.questao02;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class MainActivity extends AppCompatActivity {
+    RadioButton rb40Percent, rb45Percent, rb50Percent;
+    TextView resultText;
+    TextInputEditText salaryTextField;
+    Button resultButton;
+    Double result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,37 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        setupViews();
+    }
+
+    private void setupViews() {
+        rb40Percent = findViewById(R.id.rb40Percent);
+        rb45Percent = findViewById(R.id.rb45Percent);
+        rb50Percent = findViewById(R.id.rb50Percent);
+
+        resultText = findViewById(R.id.resultText);
+
+        salaryTextField = findViewById(R.id.salaryTextField);
+
+        resultButton = findViewById(R.id.resultButton);
+    }
+
+    public void onResultButtonClick(View view) {
+        resetResult();
+
+        Double salary = Double.parseDouble(salaryTextField.getText().toString());
+        Double multiplier = 0.0;
+
+        if (rb40Percent.isChecked()) { multiplier = 0.4; };
+        if (rb45Percent.isChecked()) { multiplier = 0.45; };
+        if (rb50Percent.isChecked()) { multiplier = 0.5; };
+
+        result = salary + (salary * multiplier);
+        resultText.setText(String.format("R$ %.2f", result).replace(".", ","));
+    }
+
+    private void resetResult() {
+        result = 0.0;
     }
 }
