f411023c7d615f5d9c9b4238c38b8109baaba9b9
joaovictorheitz <joaovictorguedes55@gmail.com>
2025-05-15 19:29:23 -0300

    questao03: removing default mainActivity

--- Questao03/app/src/main/java/com/example/questao03/WelcomeActivity.java
+++ Questao03/app/src/main/java/com/example/questao03/WelcomeActivity.java
@@ -13,8 +13,8 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.signup_activity);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signUpXML), (v, insets) -> {
+        setContentView(R.layout.welcome_activity);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.welcomeXML), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
