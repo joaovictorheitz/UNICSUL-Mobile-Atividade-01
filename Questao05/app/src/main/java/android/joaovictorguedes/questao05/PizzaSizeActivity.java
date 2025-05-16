7a21aed26f78f069e6f173b351782657bca31d22
joaovictorheitz <joaovictorguedes55@gmail.com>
2025-05-16 16:26:24 -0300

    questao05: adding functions in pizza size activity

--- Questao05/app/src/main/java/android/joaovictorguedes/questao05/PizzaSizeActivity.java
+++ Questao05/app/src/main/java/android/joaovictorguedes/questao05/PizzaSizeActivity.java
@@ -1,6 +1,9 @@
 package android.joaovictorguedes.questao05;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.radiobutton.MaterialRadioButton;
+
 public class PizzaSizeActivity extends AppCompatActivity {
+    String taste;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +25,51 @@ public class PizzaSizeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        setupTaste();
+    }
+
+    public void routeToResume(View view) {
+        Intent intent = new Intent(this, PizzaResumeActivity.class);
+
+        intent.putExtra("tasteChoosed", taste);
+        intent.putExtra("pizzaSize", getPizzaSize());
+        intent.putExtra("paymentMethod", getPaymentMethod());
+
+        startActivity(intent);
+    }
+
+    private void setupTaste() {
+        Intent intent = getIntent();
+
+        taste = intent.getStringExtra("tasteChoosed");
+    }
+
+    private int getPizzaSize() {
+        RadioGroup radioGroupSize = findViewById(R.id.radioGroup);
+        int radioButtonId = radioGroupSize.getCheckedRadioButtonId();
+        MaterialRadioButton radioButtonSizeSelected = findViewById(radioButtonId);
+
+        if (radioButtonSizeSelected == null) { return 0; }
+
+        if (radioButtonId == R.id.pizza35RadioButton) {
+            return 35;
+        } else if (radioButtonId == R.id.pizza30RadioButton) {
+            return 30;
+        } else if (radioButtonId == R.id.pizza25RadioButton) {
+            return 25;
+        }
+
+        return 0;
+    }
+
+    private String getPaymentMethod() {
+        RadioGroup radioGroupPayment = findViewById(R.id.radioGroup2);
+        int radioButtonId = radioGroupPayment.getCheckedRadioButtonId();
+        MaterialRadioButton radioButtonPaymentSelected = findViewById(radioButtonId);
+
+        if (radioButtonPaymentSelected == null) { return ""; }
+
+        return radioButtonPaymentSelected.getText().toString();
     }
 }
