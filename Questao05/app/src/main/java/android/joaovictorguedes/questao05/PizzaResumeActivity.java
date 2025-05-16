4325b47a131779def7d25859d357ea01139aa5aa
joaovictorheitz <joaovictorguedes55@gmail.com>
2025-05-16 16:05:53 -0300

    questao05: adding function in pizza taste activity

--- Questao05/app/src/main/java/android/joaovictorguedes/questao05/PizzaResumeActivity.java
+++ Questao05/app/src/main/java/android/joaovictorguedes/questao05/PizzaResumeActivity.java
@@ -1,12 +1,18 @@
 package android.joaovictorguedes.questao05;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.radiobutton.MaterialRadioButton;
 
 public class PizzaResumeActivity extends AppCompatActivity {
     @Override
--- Questao05/app/src/main/java/android/joaovictorguedes/questao05/PizzaTasteActivity.java
+++ Questao05/app/src/main/java/android/joaovictorguedes/questao05/PizzaTasteActivity.java
@@ -1,14 +1,23 @@
 package android.joaovictorguedes.questao05;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.radiobutton.MaterialRadioButton;
+
 public class PizzaTasteActivity extends AppCompatActivity {
+
+    String tasteChoosed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,32 @@ public class PizzaTasteActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void routeToSizeActivity(View view) {
+        Intent intent = new Intent(this, PizzaSizeActivity.class);
+
+        intent.putExtra("tasteChoosed", getTasteChoosed());
+
+        startActivity(intent);
+    }
+
+    private String getTasteChoosed() {
+        CheckBox calabresaCheckBox = findViewById(R.id.calabresaCheckBox);
+        CheckBox mussarelaCheckBox = findViewById(R.id.mussarelaCheckBox);
+        CheckBox margueritaCheckBox = findViewById(R.id.margueritaCheckBox);
+        CheckBox[] checkBoxes = { calabresaCheckBox, mussarelaCheckBox, margueritaCheckBox };
+        String result = "";
+
+        for (int i = 0; i < checkBoxes.length; i++) {
+            CheckBox item = checkBoxes[i];
+            String taste = item.getText().toString();
+
+            if (item.isChecked()) {
+                result += (i > 0 ? ", " : "") + taste;
+            }
+        }
+
+        return result;
     }
 }
