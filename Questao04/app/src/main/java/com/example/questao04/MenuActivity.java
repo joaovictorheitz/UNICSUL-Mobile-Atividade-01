af9111f08fe53c62e8ccf9298c6100bcc26bfb3b
joaovictorheitz <joaovictorguedes55@gmail.com>
2025-05-15 21:43:28 -0300

    questao04: adding menu activity

---
+++ Questao04/app/src/main/java/com/example/questao04/MenuActivity.java
@@ -0,0 +1,30 @@
+package com.example.questao04;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.menu_activity);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.menuActivityXML), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void routeToResume(View view) {
+//        startActivity(new Intent());
+    }
+}
