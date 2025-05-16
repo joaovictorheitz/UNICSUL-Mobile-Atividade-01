dc2b8d48038a56203deda53ff89056057895cdaf
joaovictorheitz <joaovictorguedes55@gmail.com>
2025-05-15 21:15:41 -0300

    questao04: adding actions in Welcome Activity

--- Questao04/app/src/main/java/com/example/questao04/WelcomeActivity.java
+++ Questao04/app/src/main/java/com/example/questao04/WelcomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.questao04;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,9 @@ public class WelcomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    void routeToMenu(View view) {
+//        startActivity(new Intent());
     }
 }
