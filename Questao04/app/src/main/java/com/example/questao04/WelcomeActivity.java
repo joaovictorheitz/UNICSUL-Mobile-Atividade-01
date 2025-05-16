04cc74008c16eb757c0cddb61b98ad5c13d61a32
joaovictorheitz <joaovictorguedes55@gmail.com>
2025-05-15 21:16:11 -0300

    questao04: rename main activity

--- Questao04/app/src/main/java/com/example/questao04/WelcomeActivity.java
+++ Questao04/app/src/main/java/com/example/questao04/WelcomeActivity.java
@@ -1,6 +1,5 @@
 package com.example.questao04;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -16,7 +15,7 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.welcome_activity);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
