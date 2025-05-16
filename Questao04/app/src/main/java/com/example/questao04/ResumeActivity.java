c664bf2dc0701e1fa5c9765f60a43d7e7a13c372
joaovictorheitz <joaovictorguedes55@gmail.com>
2025-05-15 21:52:16 -0300

    questao04: adding resume activity

--- Questao04/app/src/main/java/com/example/questao04/ResumeActivity.java
+++ Questao04/app/src/main/java/com/example/questao04/ResumeActivity.java
@@ -1,5 +1,6 @@
 package com.example.questao04;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,21 +10,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class ResumeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.welcome_activity);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.resume_activity);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.resumeActivityXML), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
 
-    public void routeToMenu(View view) {
+    public void routeToWelcomePage(View view) {
 //        startActivity(new Intent());
     }
 }
